05a717923c21d84ac9b45fb541cbb5e5dc66e778
Fáber Sándor <sandorwow@gmail.com>
2025-12-28 09:31:23 +0100

    Configure plugin and dependency resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NarrativeTensorExplorer" // ✅ A GitHub Repo neve lehet ez
+include(":app")
+include(":benchmark")
 
