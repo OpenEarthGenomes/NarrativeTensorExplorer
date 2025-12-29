56a3257e9167b200c80aa6b2a05593c1aeea0e28
Fáber Sándor <sandorwow@gmail.com>
2025-12-29 01:34:32 +0100

    Update meaning-kernel.cpp

--- app/src/main/cpp/meaning-kernel.cpp
+++ app/src/main/cpp/meaning-kernel.cpp
@@ -1,132 +1,83 @@
 #include <jni.h>
 #include <arm_neon.h>
+#include <vector>
+#include <cmath>
 #include <android/log.h>
 
 #define LOG_TAG "MeaningKernel"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-extern "C" {
+extern "C" JNIEXPORT jfloat JNICALL
+Java_com_meaning_app_kernel_QuantizationEngine_calculateVectorDistance(
+    JNIEnv* env, [[maybe_unused]] jobject thiz, jbyteArray v1, jbyteArray v2) {
+    
+    jbyte* data1 = env->GetByteArrayElements(v1, nullptr);
+    jbyte* data2 = env->GetByteArrayElements(v2, nullptr);
+    jsize len = env->GetArrayLength(v1);
 
-// 1. KVANTÁLT HASONLÓSÁG - NEON SIMD
-JNIEXPORT jint JNICALL
-Java_com_meaning_app_kernel_QuantizationEngine_fastNeonSimilarity(
-    JNIEnv* env, jobject thiz, jbyteArray v1, jbyteArray v2) {
+    float32x4_t sum_vec = vdupq_n_f32(0.0f);
     
-    jbyte* pV1 = env->GetByteArrayElements(v1, nullptr);
-    jbyte* pV2 = env->GetByteArrayElements(v2, nullptr);
-    jint length = env->GetArrayLength(v1);
-    
-    int32_t dotProduct = 0;
-    int i = 0;
-    
-    // NEON SIMD - 16 byte egyszerre
-    for (; i <= length - 16; i += 16) {
-        int8x16_t vec1 = vld1q_s8(pV1 + i);
-        int8x16_t vec2 = vld1q_s8(pV2 + i);
+    for (int i = 0; i <= len - 4; i += 4) {
+        int8x8_t r1 = vld1_s8(reinterpret_cast<const int8_t*>(data1 + i));
+        int8x8_t r2 = vld1_s8(reinterpret_cast<const int8_t*>(data2 + i));
         
-        // Alacsony és magas részek szorzata
-        int16x8_t prod_low = vmull_s8(vget_low_s8(vec1), vget_low_s8(vec2));
-        int16x8_t prod_high = vmull_s8(vget_high_s8(vec1), vget_high_s8(vec2));
-        
-        // Összegzés 32-bites regiszterekbe
-        int32x4_t sum_low = vaddl_s16(vget_low_s16(prod_low), vget_high_s16(prod_low));
-        int32x4_t sum_high = vaddl_s16(vget_low_s16(prod_high), vget_high_s16(prod_high));
-        
-        // Végső összeg
-        int32x4_t final_sum = vaddq_s32(sum_low, sum_high);
-        dotProduct += vaddvq_s32(final_sum);
+        int16x8_t diff = vsubl_s8(r1, r2);
+        float32x4_t diff_f = vcvtq_f32_s32(vmovl_s16(vget_low_s16(diff)));
+        sum_vec = vmlaq_f32(sum_vec, diff_f, diff_f);
     }
-    
-    // Maradék elemek
-    for (; i < length; i++) {
-        dotProduct += pV1[i] * pV2[i];
-    }
-    
-    env->ReleaseByteArrayElements(v1, pV1, JNI_ABORT);
-    env->ReleaseByteArrayElements(v2, pV2, JNI_ABORT);
-    
-    return dotProduct;
+
+    float sum = vgetq_lane_f32(sum_vec, 0) + vgetq_lane_f32(sum_vec, 1) + 
+                vgetq_lane_f32(sum_vec, 2) + vgetq_lane_f32(sum_vec, 3);
+
+    env->ReleaseByteArrayElements(v1, data1, JNI_ABORT);
+    env->ReleaseByteArrayElements(v2, data2, JNI_ABORT);
+
+    return std::sqrt(sum);
 }
 
-// 2. 3D KOORDINÁTA TRANSZFORMÁCIÓ - Mátrix szorzás NEON-al
-JNIEXPORT void JNICALL
+extern "C" JNIEXPORT void JNICALL
 Java_com_meaning_app_kernel_QuantizationEngine_transform3DCoordinates(
-    JNIEnv* env, jobject thiz,
-    jfloatArray points, jint pointCount,
-    jfloatArray matrix) {
+    JNIEnv* env, [[maybe_unused]] jobject thiz,
+    jfloatArray points, jfloatArray matrix, jint pointCount) {
     
-    jfloat* pointsPtr = env->GetFloatArrayElements(points, nullptr);
-    jfloat* matrixPtr = env->GetFloatArrayElements(matrix, nullptr);
-    
-    // Mátrix oszlopokra bontása NEON regiszterekbe
-    float32x4_t col0 = vld1q_f32(matrixPtr);
-    float32x4_t col1 = vld1q_f32(matrixPtr + 4);
-    float32x4_t col2 = vld1q_f32(matrixPtr + 8);
-    float32x4_t col3 = vld1q_f32(matrixPtr + 12);
-    
+    jfloat* p = env->GetFloatArrayElements(points, nullptr);
+    jfloat* m = env->GetFloatArrayElements(matrix, nullptr);
+
+    float32x4_t m0 = vld1q_f32(m);
+    float32x4_t m1 = vld1q_f32(m + 4);
+    float32x4_t m2 = vld1q_f32(m + 8);
+
     for (int i = 0; i < pointCount * 3; i += 3) {
-        // Pont koordinátái
-        float32x4_t point = {pointsPtr[i], pointsPtr[i+1], pointsPtr[i+2], 1.0f};
+        float32x4_t v = {p[i], p[i+1], p[i+2], 1.0f};
         
-        // Mátrix szorzás NEON-al
-        float32x4_t result;
-        result = vmulq_n_f32(col0, vgetq_lane_f32(point, 0));
-        result = vmlaq_n_f32(result, col1, vgetq_lane_f32(point, 1));
-        result = vmlaq_n_f32(result, col2, vgetq_lane_f32(point, 2));
-        result = vaddq_f32(result, col3);
-        
-        // Eredmény mentése
-        pointsPtr[i] = vgetq_lane_f32(result, 0);
-        pointsPtr[i+1] = vgetq_lane_f32(result, 1);
-        pointsPtr[i+2] = vgetq_lane_f32(result, 2);
+        p[i]   = vgetq_lane_f32(vdotq_f32(v, m0, v), 0); // Egyszerűsített vetítés
+        p[i+1] = vgetq_lane_f32(vdotq_f32(v, m1, v), 1);
+        p[i+2] = vgetq_lane_f32(vdotq_f32(v, m2, v), 2);
     }
-    
-    env->ReleaseFloatArrayElements(points, pointsPtr, 0);
-    env->ReleaseFloatArrayElements(matrix, matrixPtr, JNI_ABORT);
+
+    env->ReleaseFloatArrayElements(points, p, 0);
+    env->ReleaseFloatArrayElements(matrix, m, JNI_ABORT);
 }
 
-// 3. KVANTÁLÁS TÖMEGESEN - NEON gyorsítás
-JNIEXPORT void JNICALL
-Java_com_meaning_app_kernel_QuantizationEngine_quantizeBatch(
-    JNIEnv* env, jobject thiz,
-    jfloatArray input, jbyteArray output, jint count) {
+extern "C" JNIEXPORT jfloatArray JNICALL
+Java_com_meaning_app_kernel_QuantizationEngine_processTensor(
+    JNIEnv* env, [[maybe_unused]] jobject thiz,
+    jfloatArray input, jint rows, jint cols) {
     
-    jfloat* inputPtr = env->GetFloatArrayElements(input, nullptr);
-    jbyte* outputPtr = env->GetByteArrayElements(output, nullptr);
-    
-    const float scale = 127.0f;
-    
-    for (int i = 0; i < count; i += 16) {
-        // 4 float32 betöltése NEON regiszterekbe
-        float32x4_t vec1 = vld1q_f32(inputPtr + i);
-        float32x4_t vec2 = vld1q_f32(inputPtr + i + 4);
-        float32x4_t vec3 = vld1q_f32(inputPtr + i + 8);
-        float32x4_t vec4 = vld1q_f32(inputPtr + i + 12);
-        
-        // Skálázás és konverzió int8-ra
-        vec1 = vmulq_n_f32(vec1, scale);
-        vec2 = vmulq_n_f32(vec2, scale);
-        vec3 = vmulq_n_f32(vec3, scale);
-        vec4 = vmulq_n_f32(vec4, scale);
-        
-        // Float -> Int konverzió (egyszerűsítve)
-        int32x4_t int1 = vcvtq_s32_f32(vec1);
-        int32x4_t int2 = vcvtq_s32_f32(vec2);
-        int32x4_t int3 = vcvtq_s32_f32(vec3);
-        int32x4_t int4 = vcvtq_s32_f32(vec4);
-        
-        // Int32 -> Int8 csomagolás
-        int16x8_t short1 = vcombine_s16(vmovn_s32(int1), vmovn_s32(int2));
-        int16x8_t short2 = vcombine_s16(vmovn_s32(int3), vmovn_s32(int4));
-        int8x16_t byteVec = vcombine_s8(vmovn_s16(short1), vmovn_s16(short2));
-        
-        // Eredmény mentése
-        vst1q_s8((int8_t*)(outputPtr + i), byteVec);
-    }
-    
-    env->ReleaseFloatArrayElements(input, inputPtr, JNI_ABORT);
-    env->ReleaseByteArrayElements(output, outputPtr, 0);
-}
+    jfloat* data = env->GetFloatArrayElements(input, nullptr);
+    jfloatArray result = env->NewFloatArray(rows);
+    std::vector<float> rowSums(rows, 0.0f);
 
+    for (int i = 0; i < rows; ++i) {
+        float32x4_t sum_v = vdupq_n_f32(0.0f);
+        for (int j = 0; j <= cols - 4; j += 4) {
+            sum_v = vaddq_f32(sum_v, vld1q_f32(data + i * cols + j));
+        }
+        rowSums[i] = vgetq_lane_f32(sum_v, 0) + vgetq_lane_f32(sum_v, 1) + 
+                     vgetq_lane_f32(sum_v, 2) + vgetq_lane_f32(sum_v, 3);
+    }
+
+    env->SetFloatArrayRegion(result, 0, rows, rowSums.data());
+    env->ReleaseFloatArrayElements(input, data, JNI_ABORT);
+    return result;
 }
